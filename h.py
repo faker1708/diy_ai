f47815f73a945c808b09e5178a00965517294b96
faker1708 <71315741+faker1708@users.noreply.github.com>
2023-03-17 16:24:00 +0800

    大概成功了，,恢复一个网络的参数

--- h.py
+++ h.py
@@ -1,5 +1,5 @@
-import torch
-
+impor torch
+import math
 
     # x = torch.normal(0,1,(n,1)).half().cuda()
     # w = torch.normal(0,1,(m,n),requires_grad=True).half().cuda()
@@ -7,10 +7,8 @@ import torch
 
 rl = torch.nn.ReLU(inplace=False)
 
-def fa():
+def get_data_set(n,m):
     
-    n = 1+3
-    m = 1+2
 
 
 
@@ -18,7 +16,6 @@ def fa():
     true_w = torch.normal(0,1,(m,n))
     true_w [-1]=0
     true_w [-1,-1]=1
-    true_w = true_w
 
 
     batch_width = 100
@@ -44,19 +41,86 @@ def fa():
 
     ds = list()
     for i in range(batch_high):
-        for j in range(batch_high):
+        for j in range(batch_width):
             data = list()
             x = torch.normal(0,1,(n,1))
             x[-1]=1
+            # y = true_w @ x
             y = rl(true_w @ x)
             data.append(x)
             data.append(y)
             ds.append(data)
         
+    print(len(ds))
+    return ds
 
+
+def loss_f(y,true_y):
+    # print('输出一个标量')
+    yy = y-true_y
+
+    loss_tensor = yy**2/2
+
+    # print('y',y)
+
+    # print('loss',loss_tensor)
+
+    loss = loss_tensor.sum()
+
+    return loss
+
+
+def fa():
+    n = 1+12
+    m = 1+3
+    ds = get_data_set(n,m)
     print(ds[0])
+    print(len(ds))
+
+    # d2l.set_figsize()
+    # d2l.plt.scatter(features[:, 1].detach().numpy(), labels.detach().numpy(), 1)
 
 
+    w = torch.normal(0,1,(m,n)).half().cuda()
+    w [-1]=0
+    w [-1,-1]=1
+    w.requires_grad=True
+
+    
+    epoch = 30
+    lr = 0.01
+
+    for j in range(epoch):
+
+        for i in range(1000):
+            data = ds[i]
+            x = data[0].cuda().half()
+            true_y = data[1].cuda().half()
+            # print(x)
+            # print(true_y)
+
+
+            
+            # y =w @ x
+            y =rl( w @ x)
+
+            loss = loss_f(y,true_y)
+
+            loss.backward(retain_graph=True)
+
+            with torch.no_grad():
+                w -= lr * w.grad 
+                w.grad.zero_()
+
+        ll = float(loss)
+        # print(ll)
+        p = -math.log(ll)
+        print(p)
+            # print(math.ceil(p))
+            # break
+
+
+    
 
 if __name__ == "__main__":
     fa()
